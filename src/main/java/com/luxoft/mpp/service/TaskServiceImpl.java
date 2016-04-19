e8c7dae29dd94c44f1b2d373fa380a21b1515e1e
iivaniv <ivanyv.ivan@yandex.ua>
2016-04-19 19:59:36 +0300

    update task graph

--- src/main/java/com/luxoft/mpp/service/TaskServiceImpl.java
+++ src/main/java/com/luxoft/mpp/service/TaskServiceImpl.java
@@ -1,15 +1,9 @@
 package com.luxoft.mpp.service;
 
-import com.luxoft.mpp.dao.TaskDao;
-import com.luxoft.mpp.dao.TaskGraphDao;
-import com.luxoft.mpp.entity.model.Etl;
-import com.luxoft.mpp.entity.model.TaskVertex;
 import org.primefaces.model.diagram.Element;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
+import java.util.*;
 
 /**
  * Created by iivaniv on 22.03.2016.
@@ -18,21 +12,60 @@ import java.util.List;
 @Service("taskVertexServiceImpl")
 public class TaskServiceImpl implements TaskService {
 
+    private int[][] lm = new int[0][0];
+    private Integer[] vertex = new Integer[0];
+
+
+
 //    @Autowired
 //    private TaskDao taskGraphDao;
 
     public void saveVertex(List<Element> elements){
 
-//        Etl etl = new Etl();
-//        for (Element e : elements ){
-//
-//            TaskVertex vertex = new TaskVertex();
-//
-//            vertex.setEtl(etl);
-//
-//            taskGraphDao.save(vertex);
-//        }
 
+    }
+
+
+    public void updateVertex( int duration ){
+
+        List<Integer> v = Arrays.asList(vertex);
+        v.add(duration);
+        v.toArray(vertex);
+
+    }
+
+    public void updateTaskModel(int from, int to, int value){
+
+        if ( value < 0){
+            throw new IllegalArgumentException(value +" can`t be lover zero ");
+        }
+
+        if ( from <= lm[0].length && from >= lm.length){
+            throw new IllegalArgumentException(from +" out of array ");
+        }
+
+        if ( to <= lm[0].length && to >= lm.length){
+            throw new IllegalArgumentException(to +" out of array ");
+        }
+
+        lm[from][to] = value;
+
+    }
+
+
+    public void isLoop(){
+
+        for( int i = 0; i < lm.length; i ++ ){
+
+            for ( int j  = 0; j < lm[i].length; j++ ){
+
+                if (lm[i][j] != 0){
+                    
+                }
+
+            }
+
+        }
 
     }
 
